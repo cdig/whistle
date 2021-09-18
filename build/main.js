1607a496a5d473022ed4d7a36e1c28b23c1afbeb
Ivan Reese <ivanreese@gmail.com>
2021-09-17 20:10:28 -0600

    Tweaking build scripts to better match Hyperzine

--- build/main.js
+++ build/main.js
@@ -1,19 +1,18 @@
-"use strict";
-
-const { app, BrowserWindow, session } = require("electron");
-const path = require("path");
-require("electron-reload")(__dirname);
+var { app, BrowserWindow } = require("electron");
 
 app.on("ready", () => {
-  const win = new BrowserWindow({
+  new BrowserWindow({
     width: 1707,
     height: 960,
+    titleBarStyle: "hiddenInset",
     backgroundColor: "#fff",
     webPreferences: {
       contextIsolation: true,
       nodeIntegration: false
     }
-  });
-  win.loadFile("build/index.html");
-  win.webContents.openDevTools();
+  }).loadFile("build/index.html");
+});
+
+app.on("window-all-closed", () => {
+  app.quit();
 });
